58d0d2e60b6edaca1376db112fb7bb83ce90d808
Mohamed Tawfik <mohamed.tawfik@secusmart.de>
2025-06-10 16:14:27 +0200

    adding frontend folder

---
+++ frontend/components/ReactMuiDemoWrapper.js
@@ -0,0 +1,13 @@
+// src/components/ReactMuiDemoWrapper.js
+import React from 'react';
+import ReactMuiDemoApp from 'db-ui'; // Adjust the import path as needed
+
+const ReactMuiDemoWrapper = () => {
+  return (
+    <div>
+      <ReactMuiDemoApp />
+    </div>
+  );
+};
+
+export default ReactMuiDemoWrapper;
